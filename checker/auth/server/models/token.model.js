e2e95437a5a4340552923ffd2da677989e50b0d7
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-02-28 10:28:57 +0300

    delete conf

--- checker/auth/server/models/token.model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const TokenSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  refreshToken: { type: String, require: true },
-  isActivated: { type: Boolean, default: false },
-});
-
-module.exports = model("Token", TokenSchema);
