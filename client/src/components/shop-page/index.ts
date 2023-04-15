dd08983d46159971529d5b8e3ce1736329dd18e6
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-04-15 10:37:57 +0300

    Add searching field for products to Admin

--- client/src/components/shop-page/index.ts
+++ client/src/components/shop-page/index.ts
@@ -2,5 +2,6 @@ import OrderCardBtn from './OrderCardBtn/OrderCardBtn';
 import ShopFilterList from './ShopFilterList/ShopFilterList';
 import TypeBar from './modules/TypeBar/TypeBar';
 import ImgCarousel from './ImgCarousel/ImgCarousel';
+import Card from './Card/Card';
 
-export { OrderCardBtn, ShopFilterList, TypeBar, ImgCarousel };
+export { OrderCardBtn, ShopFilterList, TypeBar, ImgCarousel, Card };
--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { SortProps } from '@/types/uiTypes';
-import { SortTypes } from '@/types/productTypes';
-import { IconButton, Loader } from '@/components/ui';
+import { FnOnChange, SortProps } from '@/types/uiTypes';
+import { IProduct, SortTypes } from '@/types/productTypes';
+import { IconButton, Loader, TextInput } from '@/components/ui';
 import { EntityContainer, AdminItemForList, EditItemModule } from '@/components/admin-page';
 import httpService from '@/http/productAPI';
 import { frontDataAdapter } from '@/utils';
@@ -16,6 +16,7 @@ const Admin = observer(() => {
   const [isLoading, setIsLoading] = useState(false);
   const [show, setShow] = useState(false);
   const [updated, setUpdated] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
   const [sortType, setSortType] = useState<SortProps>({
     type: 'type',
     sort: 'asc'
@@ -37,13 +38,24 @@ const Admin = observer(() => {
         }
       })();
     }
-  }, [products, updated]);
+  }, [updated]);
 
   useEffect(() => {
     if (Array.isArray(products.products)) {
       products.productSorting(sortType.type, sortType.sort);
     }
   }, [sortType]);
+
+  // useEffect(() => {
+
+  // }, [searchQuery]);
+
+  const handleSearch: FnOnChange = ({ value }) => {
+    if (typeof value !== 'string') {
+      return;
+    }
+    setSearchQuery(value);
+  };
 
   const onClick = (type: keyof SortTypes) => {
     const sort = changeProductSortWay(type, sortType);
@@ -53,6 +65,17 @@ const Admin = observer(() => {
   const onHide = () => {
     setShow(!show);
   };
+
+  function searchItems(data: IProduct[]) {
+    const filteredData = searchQuery
+      ? data.filter((product) => product.sortName.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1)
+      : data;
+
+    return filteredData;
+  }
+
+  const filteredItems = searchItems(products.products);
+
   return (
     <main className={styles.admin}>
       <div className={styles.admin_types}>
@@ -69,6 +92,7 @@ const Admin = observer(() => {
       </div>
       <hr />
       <div className={styles.admin_items}>
+        <TextInput name="searchQuery" value={searchQuery} onChange={handleSearch} placeholder="Поиск по названию" />
         <div className={styles.items_fields}>
           {ADMIN_ITEM_FIELDS.map((i) => (
             <div onClick={() => onClick(i.type)} key={i.id}>
@@ -77,7 +101,7 @@ const Admin = observer(() => {
           ))}
           <IconButton appearance="primary" onClick={onHide} icon="Add" />
         </div>
-        {isLoading ? <Loader /> : products.products.map((i) => <AdminItemForList key={i.id} product={i} />)}
+        {isLoading ? <Loader /> : filteredItems.map((i) => <AdminItemForList key={i.id} product={i} />)}
       </div>
       {show && <EditItemModule onUpdated={setUpdated} onHide={onHide} />}
     </main>
