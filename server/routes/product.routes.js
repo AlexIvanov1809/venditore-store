26a8ada67216375b2d156e6cb8121882071ed0a8
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-04-29 15:11:20 +0300

    Add authMiddleware for product routes

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,11 +1,11 @@
 const Router = require('express');
 const router = new Router();
 const productController = require('../controllers/product.controller');
-const checkRole = require('../middleware/CheckRole.middleware');
+const authMiddleware = require('../middleware/CheckRole.middleware');
 
-router.post('/', productController.create);
-router.delete('/:id', productController.delete);
-router.patch('/:id', productController.edit);
+router.post('/', authMiddleware(), productController.create);
+router.delete('/:id', authMiddleware(), productController.delete);
+router.patch('/:id', authMiddleware(), productController.edit);
 router.get('/', productController.getAll);
 router.get('/search', productController.search);
 router.get('/:id', productController.getOne);
