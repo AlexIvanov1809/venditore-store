d954d92f0bfc8b020552503503a53203346957ee
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-04-29 15:13:02 +0300

    Add authMiddleware for user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,9 +2,11 @@ const Router = require('express');
 const router = new Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middleware/CheckRole.middleware');
 
 router.post(
   '/registration',
+  // authMiddleware(),
   body('email').isEmail().withMessage('Указан неверный email'),
   body('password')
     .isLength({ min: 8, max: 32 })
