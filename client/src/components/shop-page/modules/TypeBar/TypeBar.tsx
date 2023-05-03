f320599d6724ce991089d40ea11df10536c99e9e
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-05-03 10:12:30 +0300

    Add visibility for types which has products

--- client/src/components/shop-page/modules/TypeBar/TypeBar.tsx
+++ client/src/components/shop-page/modules/TypeBar/TypeBar.tsx
@@ -2,6 +2,8 @@ import cn from 'classnames';
 import { useRootStore } from '@/context/StoreContext';
 import { Button } from '@/components/ui';
 import styles from './TypeBar.module.scss';
+import { useEffect, useState } from 'react';
+import httpService from '@/http/productAPI';
 
 interface Props {
   className: string;
@@ -9,19 +11,38 @@ interface Props {
 
 function TypeBar({ className }: Props) {
   const { products } = useRootStore();
+  const [allTypes, setAllTypes] = useState<null | { [key: string]: boolean }>(null);
+
+  useEffect(() => {
+    const types = products.types;
+    types.forEach(async (type) => {
+      try {
+        const { count } = await httpService.fetchProducts({ typeId: type.id, page: 1, limit: 1 });
+        if (count) {
+          setAllTypes((prev) => ({ ...prev, [type.name]: true }));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, [products.types]);
   return (
     <div className={cn(className, styles.item_container)}>
-      {products.types.map((type) => (
-        <Button
-          appearance="primary"
-          className={styles.item}
-          key={type.id}
-          onClick={() => products.setSelectedType(type.id ? type.id : '')}
-          data-active={type.id === products.selectedType}
-        >
-          {type.name}
-        </Button>
-      ))}
+      {products.types.map((type) => {
+        if (allTypes && allTypes[type.name]) {
+          return (
+            <Button
+              appearance="primary"
+              className={styles.item}
+              key={type.id}
+              onClick={() => products.setSelectedType(type.id ? type.id : '')}
+              data-active={type.id === products.selectedType}
+            >
+              {type.name}
+            </Button>
+          );
+        }
+      })}
     </div>
   );
 }
